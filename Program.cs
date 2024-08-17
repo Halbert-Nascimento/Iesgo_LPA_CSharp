43cda73af8fe9ef4136a5685d8548258467de928
Halbert Nascimento <halbert.fac@gmail.com>
2024-08-16 23:51:35 -0300

    Atividade aula 14/08 finalizada

--- Program.cs
+++ Program.cs
@@ -1,1 +1,201 @@
-﻿
+﻿//Aula 14/08/2024
+
+//Aluno: Halbert Nascimento
+
+//Prof: Romes
+
+
+using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace Aula_14_08
+{
+
+    public class Par
+    {
+        public void MetodoPar()
+        {
+            Console.Write("Digite um numero: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite + um numero: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int soma = num1 + num2;
+
+            Console.WriteLine("A soma dos numero é: " + soma);
+
+            //usando if e else
+            if (soma % 2 == 0)
+            {
+                Console.WriteLine("A soma dos numeros e par");
+            }
+            else
+            {
+                Console.WriteLine("A soma dos numeros e Impar");
+            }
+        }
+    }
+
+    public class Tabuada
+    {
+        public void MetodoTab()
+        {
+
+            Console.Write("Digite um numero para tabuada: ");
+            int tab = Convert.ToInt32(Console.ReadLine());
+
+            //usando for loop
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"Tabuada de  {tab} X {i} = {tab * i}");
+            }
+
+        }
+    }
+
+    class Contador
+    {
+        public void metodoContador()
+        {
+            Console.WriteLine("Contador: ");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+        }
+    }
+
+    class MediaNotas
+    {
+        public void Metodo_mediaNotas()
+        {
+            double[] notas = new double[3];
+            double somaM = 0;
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.Write($"Digite a nota {1}: ");
+                notas[i] = Convert.ToInt32(Console.ReadLine());
+                somaM += notas[i];
+            }
+            Console.WriteLine($"A media das notas e: {somaM / notas.Length} ");
+
+        }
+    }
+
+    public class Atv_in_Aula
+    {
+        public void atv()
+        {
+            //Você vai criar um programa que calcula o salário de um 
+            //funcionário com base nas horas trabalhadas, na taxa de 
+            //pagamento por hora e no valor das comissões.O programa 
+            //deve usar diferentes tipos de dados para armazenar as 
+            //informações e realizar as operações necessárias.
+            //1.Solicite ao usuário para inserir o nome do funcionário(string).
+            //2.Solicite ao usuário para inserir as horas trabalhadas na semana(int).
+            //3.Solicite ao usuário para inserir a taxa de pagamento por hora(double).
+            //4.Solicite ao usuário para inserir o valor das comissões recebidas na semana(decimal).
+            //5.Calcule o salário bruto(horas trabalhadas multiplicadas pela taxa de pagamento) e adicione as comissões.
+            //6.Exiba o nome do funcionário e o salário total(decimal) no final.
+
+            //1 - 1.Solicite ao usuário para inserir o nome do funcionário(string).
+            Console.Write("Digite o nome do funcionario: ");
+            String nomeFuncionario = Console.ReadLine();
+
+            //2 - 2.Solicite ao usuário para inserir as horas trabalhadas na semana(int).
+            Console.Write("Informe as horas trabalhadas: ");
+            int horasTrabalhadas = Convert.ToInt32(Console.ReadLine());
+
+            // 3 - Solicite ao usuário para inserir a taxa de pagamento por hora(double)
+            Console.Write("Informe a taxa de pagamento hora: ");
+            double taxaPagamento = Convert.ToDouble(Console.ReadLine());
+
+            // 4 - Solicite ao usuário para inserir o valor das comissões recebidas na semana(decimal).
+            Console.Write("Informe o valor das comisssoes recebina na semana: ");
+            decimal comissao = Convert.ToDecimal(Console.ReadLine());
+
+            //5 - Calcule o salário bruto(horas trabalhadas multiplicadas pela taxa de pagamento) e adicione as comissões.
+            decimal salarioBruto = (decimal)(horasTrabalhadas * taxaPagamento) + comissao;
+
+            //6 - Exiba o nome do funcionário e o salário total(decimal) no final.
+            Console.WriteLine($"{nomeFuncionario} seu salario e: {salarioBruto}");
+
+
+        }
+    }
+
+    class RepresentacaoDeCaracteres
+    {
+        public void MetodoAtv2()
+        {
+            //crien um programa em C# que peça ao usuario inserir um caracter e exiba em 
+            //seguida esse caractere de quatro encoding diferentes.
+
+            //1. literal de caractere
+            //2. unicode
+            //3. Hexadecimal
+            //4. Inteiro para char
+
+            //Solicitar que o suario insira um caracter
+            Console.Write("Digite um caractere: ");
+            char caractere = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            //Converter o caracter conforme o enunciado
+            string unicodeRepresentacao = $"\\u{(int)caractere:X4}";
+
+            string hexadecimaRepresentacao = $"\\x{(int)caractere:X4}";
+
+            int inteiroRepresentacao = (int)caractere;
+
+            // exibir as representações
+            Console.WriteLine($"Caractere litera:' {caractere}'");
+            Console.WriteLine($"Caractere Unicode: '{unicodeRepresentacao}'");
+            Console.WriteLine($"Caractere HexaDecimal: '{hexadecimaRepresentacao}'");
+            Console.WriteLine($"Caractere Inteiro: '{inteiroRepresentacao}'");
+
+
+
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Par objTeste = new Par();
+            //objTeste.MetodoPar();
+
+            Tabuada objTabuada = new Tabuada();
+            //objTabuada.MetodoTab();
+
+            Contador objContador = new Contador();
+            //objContador.metodoContador();
+
+            MediaNotas objMediaNotas = new MediaNotas();
+            //objMediaNotas.Metodo_mediaNotas();
+
+
+            Atv_in_Aula atividade = new Atv_in_Aula();
+            //atividade.atv();
+
+            RepresentacaoDeCaracteres atividade2 = new RepresentacaoDeCaracteres();
+            //atividade2.MetodoAtv2();
+
+            // Classe SecretariaEscolar esta em outro arquivo de classe SecretariaEscolar.cs
+            SecretariaEscolar secretariaEscolar = new SecretariaEscolar();
+            secretariaEscolar.Turma();
+
+
+            //Fim foi ate o 10 do main
+        }
+    }
+
+
+    //fim nameSpace aula
+}
---
+++ SecretariaEscolar.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+
+public class SecretariaEscolar
+{
+    static void calcularMediaDeNotas(List<decimal> notasPortugues, List<decimal> notasMatematica)
+    {
+        decimal somaNotaPorgues = 0;
+        decimal somaMatematica = 0;
+        foreach (int notaP in notasPortugues)
+        {
+            somaNotaPorgues += notaP;
+        }
+        foreach (int notaM in notasMatematica)
+        {
+            somaMatematica += notaM;
+        }
+        decimal mediaMatemtica = somaMatematica / notasMatematica.Count;
+        decimal mediaPortugues = somaNotaPorgues / notasPortugues.Count;
+
+        Console.Write($"A media das notas de PORTUGUES de todos os alunos e: {mediaPortugues}");
+        Console.WriteLine();
+        Console.Write($"A media das notas de MATEMATICA de todos os alunos e: {mediaMatemtica}");
+        Console.WriteLine();
+
+    }
+    static string RepeatCaracterFim(string caracter, int vezes)
+    {
+        string repeticaoString = caracter;
+        for (int i = 0; i < vezes; i++)
+        {
+            repeticaoString += caracter;
+
+        }
+
+
+        return repeticaoString += "\n\n";
+    }
+
+    static void FormatCorTitulo()
+    {
+        Console.ResetColor();
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+
+    }
+    static void ExibirDiferentesRepresentacoes(string texto)
+    {
+        FormatCorTitulo();
+        Console.Write("Exibindo o nome do aluno em diferentes representações:");
+        Console.ResetColor();
+        Console.WriteLine(); // salta 1 linha
+
+        //converte a string texto para um array de caracter
+        char[] arr_char = texto.ToCharArray();
+
+        //exibe os caracter de forma literal com tabulação
+        Console.Write("{0,-20}", "Literal: ");
+        for (int caracter = 0; caracter < arr_char.Length; caracter++)
+        {
+            Console.Write("{0,-10}", arr_char[caracter]);
+        }
+        Console.WriteLine(); // pular linha
+
+        //exibir caracteres unicode com tabulação
+        Console.Write("{0,-20}", "Unicode: ");
+        for (int caracter = 0; caracter < arr_char.Length; caracter++)
+        {
+            string unicodeRepresentacao = $"\\u{(int)arr_char[caracter]:X4}";
+            Console.Write("{0,-10}", unicodeRepresentacao);
+        }
+        Console.WriteLine(); // pular linha
+
+
+        //exibe os caracter hexadecima com tabulação
+        Console.Write("{0,-20}", "HexaDecimal: ");
+        for (int caracter = 0; caracter < arr_char.Length; caracter++)
+        {
+            string hexadecimaRepresentacao = $"\\x{(int)arr_char[caracter]:X4}";
+            Console.Write("{0,-10}", hexadecimaRepresentacao);
+        }
+        Console.WriteLine(); // pular linha
+
+        // exibir caracter inteiro com tabulação
+        Console.Write("{0,-20}", "Decimal: ");
+        for (int caracter = 0; caracter < arr_char.Length; caracter++)
+        {
+            int inteiroRepresentacao = (int)arr_char[caracter];
+            Console.Write("{0,-10}", inteiroRepresentacao);
+        }
+        Console.WriteLine(); // pular linha
+
+    }
+
+    static void ExibirSeFoiAprovado(decimal notaPortugues, decimal notaMatematica)
+    {
+
+        Console.BackgroundColor = notaPortugues >= 60 ? ConsoleColor.Green : ConsoleColor.Red;
+        Console.Write(notaPortugues >= 60 ? "APROVADO EM PORTUGUES!" : "REPROVADO EM PORTUGUES!");
+        Console.ResetColor();
+
+        Console.WriteLine(); // salta 1 linha
+
+        Console.BackgroundColor = notaMatematica >= 60 ? ConsoleColor.Green : ConsoleColor.Red;
+        Console.Write(notaMatematica >= 60 ? "APROVADO EM MATEMATICA!" : "REPROVADO EM MATEMATICA!");
+        Console.ResetColor();
+        Console.WriteLine(); // salta 1 linha
+
+
+    }
+    static void ValidarBolsaEstudor(decimal notaPortugues, decimal notaMatematica)
+    {
+        Console.BackgroundColor = notaPortugues < 80 || notaMatematica < 80 ? ConsoleColor.Red : ConsoleColor.Green;
+        Console.Write(notaPortugues < 80 || notaMatematica < 80 ? "Devido nota inferior a 80. O aluno perdeu a bolsa de estudos!" : "Aluno mantem a bolsa de estudos!");
+        Console.ResetColor();
+        Console.WriteLine(); // salta 1 linha
+
+
+    }
+
+    static void ExibirAlunos(List<string> nomes, List<int> idades, List<decimal> notasPortugues, List<decimal> notasMatematica, List<Boolean> bolsaEstudos)
+    {
+
+        Console.WriteLine(); //quebrar linha
+        //formatar a cor do console
+        Console.BackgroundColor = ConsoleColor.DarkYellow;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("\t\t\t ****** Alunos ******");
+        Console.ResetColor(); // reseta a cor do console para original
+        Console.WriteLine(); //quebrar linha
+
+        for (int aluno = 0; aluno < nomes.Count; aluno++)
+        {
+            Console.WriteLine($"Aluno: {nomes[aluno]}, idade {idades[aluno]}, possui a nota {notasPortugues[aluno]} em portugues e {notasMatematica[aluno]} em matematica, ");
+
+            ExibirSeFoiAprovado(notasPortugues[aluno], notasMatematica[aluno]);
+
+            if (bolsaEstudos[aluno])
+            {
+                ValidarBolsaEstudor(notasPortugues[aluno], notasMatematica[aluno]);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine("Não possui bolsa de estudos!");
+
+            }
+
+            ExibirDiferentesRepresentacoes(nomes[aluno]);
+
+
+            Console.Write(RepeatCaracterFim("#", 50));
+        }
+
+        calcularMediaDeNotas(notasPortugues, notasMatematica);
+
+        Console.Write(RepeatCaracterFim("_", 100));
+
+    }
+
+    public void Turma()
+    {
+
+        List<string> nomes = new List<string>();
+        List<int> idades = new List<int>();
+        List<decimal> notasPortugues = new List<decimal>();
+        List<decimal> notasMatematica = new List<decimal>();
+        List<Boolean> bolsaEstudos = new List<Boolean>();
+
+        string sair = "";
+        int cont = 0;
+        while (sair != "f")
+        {
+            Console.Clear();
+            Console.WriteLine($"Chamada numero: {cont}");
+
+            Console.Write($"Digite o nome do aluno: ");
+
+            string nome = Console.ReadLine();
+
+
+            Console.Write($"Digite a idade de {nome}: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Digite a nota de PORTUGUES do aluno {nome}: ");
+            decimal notaPortugues = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write($"Digite a nota de MATEMATICA do aluno {nome}: ");
+            decimal notaMatematica = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write($"{nome} possui bolsa de estudos? sim/nao ");
+            string bolsaEstudo = Console.ReadLine().ToLower();
+            Boolean bolsa_estudo_bool = (bolsaEstudo == "sim") ? true : false;
+
+            nomes.Add(nome);
+            idades.Add(idade);
+            notasPortugues.Add(notaPortugues);
+            notasMatematica.Add(notaMatematica);
+            bolsaEstudos.Add(bolsa_estudo_bool);
+
+            Console.WriteLine("Cadastrar novo aluno? ");
+            FormatCorTitulo();
+            Console.Write("   Pressione qualquer tecla p continuar ou 'F' para finalizar: ");
+            Console.ResetColor();
+            cont++;
+            sair = Console.ReadLine().ToLower();
+
+        }
+        Console.Clear();
+        ExibirAlunos(
+            nomes, idades, notasPortugues, notasMatematica, bolsaEstudos
+            );
+
+    }
+}
